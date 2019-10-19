2e0d3be8b5fe24ae79c1abe7cbfe16c25319430a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-19 10:59:25 +0100

    Refined now make sense!

--- src/test/scala/img/BuildUpdate.scala
+++ src/test/scala/img/BuildUpdate.scala
@@ -1,8 +1,10 @@
 package img
 
-import eu.timepit.refined.boolean._
-import eu.timepit.refined._
-import eu.timepit.refined.numeric._
+import eu.timepit.refined.W
+import eu.timepit.refined.boolean.{And, Not}
+import eu.timepit.refined.numeric.{Greater, Less, Positive}
+import eu.timepit.refined.auto._
+import eu.timepit.refined.api.Refined
 
 object Build{
   type OnSeconds = Not[Less[W.`0`.T]] And Not[Greater[W.`4095`.T]] // [0, 2^12-1]
@@ -11,11 +13,11 @@ object Build{
   type PointsScored = Not[Less[W.`1`.T]] And Not[Greater[W.`3`.T]] // [1, 3]
 
   case class Update(
-                  matchtime: OnSeconds,
-                  team1PointsTotal: TeamPointsTotal,
-                  team2PointsTotal: TeamPointsTotal,
-                  whoScored: WhoScored,
-                  pointsScored: PointsScored
+                  matchtime: Int Refined OnSeconds,
+                  team1PointsTotal: Int Refined TeamPointsTotal,
+                  team2PointsTotal: Int Refined TeamPointsTotal,
+                  whoScored: Int Refined WhoScored,
+                  pointsScored: Int Refined PointsScored
   )
 
   def any(): Update = {
