f8c9cd3e09c6a77bd93353cc5405f2cd33502c9a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-10-21 23:29:55 +0100

    simplified

--- src/test/scala/MapperEventSpec/OnProcessBinaryStreamSpec.scala
+++ src/test/scala/MapperEventSpec/OnProcessBinaryStreamSpec.scala
@@ -5,20 +5,11 @@ import org.scalatest.FunSuite
 
 class OnProcessBinaryStreamSpec extends FunSuite {
   test("I can split in a recursive way"){
-    val result1 = MapperEvent.processBinaryStream("xxxxxxxxxxxddddddddccccccccbaa", List(2, 1, 8, 8))
-    val result2 = MapperEvent.processBinaryStream("xxxxxxxxxxxxxxxxxxxxxxxxddddddddccccccccbaa", List(2, 1, 8, 8))
-    val result3 = MapperEvent.processBinaryStream("xxxxxxxxxxxxxxxxxxxxxxxxddddddddccccccccbaa", List(1, 2, 3, 4, 5, 8))
+    val result1 = MapperEvent.processBinaryStream("xxxxxxxxxxxddddddddccccccccbaa")
+    val result2 = MapperEvent.processBinaryStream("xxxxxxxxxxxxxxxxxxxxxxxxddddddddccccccccbaa")
 
     assert(List("aa", "b", "cccccccc", "dddddddd", "xxxxxxxxxxx") == result1)
     assert(List("aa", "b", "cccccccc", "dddddddd", "xxxxxxxxxxxxxxxxxxxxxxxx") == result2)
-    assert(List("a", "ba", "ccc", "cccc", "ddddc", "xxxxdddd", "xxxxxxxxxxxxxxxxxxxx") == result3)
-  }
-
-  test("by default is using fixed indexes to split the binary stream"){
-    val result1 = MapperEvent.processBinaryStream("xxxxxxxxxxxddddddddccccccccbaa", List(2, 1, 8, 8))
-    val result2 = MapperEvent.processBinaryStream("xxxxxxxxxxxddddddddccccccccbaa")
-
-    assert(result1 == result2)
   }
 
   test("Edge case"){
